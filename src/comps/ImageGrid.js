4325436f261b469876e045c5970e9f7a25bc795b
rashidfirhat98 <rashidfirhat98@gmail.com>
2022-01-27 15:51:29 +0800

    fixed layout bug

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -10,16 +10,16 @@ const ImageGrid = ({ setSelectedImg }) => {
   return (
     <div className="img-grid">
       { docs && docs.map(doc => (
-        <motion.div className="img-wrap" key={docs.id}
-          layout 
-          whileHover={{ opacity: 1 }}
+        <motion.div className="img-wrap" key={doc.id}
+          layout
+          whileHover={{ opacity: 1 }}s
           onClick={() => setSelectedImg(doc.url)}
         >
           <motion.img src={doc.url} alt="uploaded pic" 
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ delay: 1 }}/>
-            
+            transition={{ delay: 1 }}
+          />
         </motion.div>
       ))}
     </div>
